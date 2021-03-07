8e72f2a63e7f5533190e62816a1dd4b27f1cebce
Alex Greco <alexgreco@Alexs-MBP.fios-router.home>
2021-03-07 10:26:35 -0500

    Added an express route to return the number of cpus and the average load.

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const os = require("os");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Datadog!");
+});
+
+app.get("/cpu-load", (req, res) => {
+  const cpus = os.cpus().length;
+  const loadAverage = os.loadavg()[0] / cpus;
+
+  res.send({ cpus: cpus, loadAverage: loadAverage });
 });
 
 app.listen(port, () => {
